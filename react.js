dd5071e6ed553062965ea8a7767f754335d94911
Hoishin <hoishinxii@gmail.com>
2019-07-18 12:09:35 +0900

    Disable prop-types

--- react.js
+++ react.js
@@ -5,6 +5,7 @@ module.exports = {
 	plugins: ['react-hooks'],
 	extends: ['plugin:react/recommended', 'prettier/react'],
 	rules: {
+		'react/prop-types': 0,
 		'react-hooks/rules-of-hooks': 2,
 		'react-hooks/exhaustive-deps': 2,
 	},
