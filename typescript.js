7c414e8c615129db8242d94dbe93c7852273e6c6
Keiichiro Amemiya <hoishinxii@gmail.com>
2020-11-13 10:37:17 -0800

    fix(typescript): disable import/no-unresolved in typescript config

--- typescript.js
+++ typescript.js
@@ -8,5 +8,6 @@ module.exports = {
 		'no-undef': 0, // TypeScript has it's own way to detect undefined variables
 		'react/prop-types': 0, // TypeScript does this job
 		'import/default': 0, // Conflicts with TypeScript esModuleInterop
+		'import/no-unresolved': 0, // TypeScript does this job
 	},
 };
